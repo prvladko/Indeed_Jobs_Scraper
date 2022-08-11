3cf2d7e4b98e400eb42949b9ab8b9dc7e9f57a3e
prvladko <prvladko@gmail.com>
2022-08-11 12:10:09 +0300

    Main Initial commit

--- scraper.py
+++ scraper.py
@@ -1,11 +1,7 @@
+# import os
 import requests
-from bs4 import BeautifulSoup
-
-def extract(page):
-#    headers = {'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'}
-    headers = {'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'}
-    url = f'https://ua.indeed.com/jobs?q=python+developer&l=Kyiv&start={page}'
-    r = requests.get(url, headers)
-    return r.status_code
-
-print(extract(0))
+from bs4 import BeautifulSoup # webscrape
+from collections import defaultdict # default dictionary: store a list with each key
+import pandas as pd # DF
+import re           # regular expressions
+import datetime     # format date/time
